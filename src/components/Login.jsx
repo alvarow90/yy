7d242b13bc5f5d175243b33f3f296ceb334a400b
alvarow90 <alvarogallardo18@ciencias.unam.mx>
2023-12-02 23:25:23 -0600

    estilos

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,70 @@
-// src/components/Login.jsx
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { signInWithGoogle } from '../firebase/firebaseUtils'; // Asegúrate de que la ruta es correcta
-import './home.css';
+import { signInWithGoogle } from '../firebase/firebaseUtils'; // Verifica que la ruta sea correcta
+
 export const Login = () => {
-  let navigate = useNavigate(); // Hook para la navegación
+  let navigate = useNavigate();
 
   const handleGoogleSignIn = async () => {
     try {
       await signInWithGoogle();
-      navigate('/pokedex'); // Redirige a la Pokedex después de un inicio de sesión exitoso
+      navigate('/pokedex'); // Navega a la Pokedex después de un inicio de sesión exitoso
     } catch (error) {
       console.error('Error al iniciar sesión: ', error);
     }
   };
 
+  // Estilos definidos dentro del componente
+  const styles = {
+    container: {
+      backgroundColor: 'black',
+      color: 'white',
+      textAlign: 'center',
+      padding: '2rem',
+      borderRadius: '8px',
+      maxWidth: '400px',
+      margin: '2rem auto',
+    },
+    title: {
+      marginBottom: '1rem',
+      fontSize: '2rem',
+    },
+    description: {
+      marginBottom: '2rem',
+      fontSize: '1rem',
+      lineHeight: 1.5,
+    },
+    button: {
+      backgroundColor: '#646cff',
+      color: 'white',
+      padding: '0.6em 1.2em',
+      border: 'none',
+      borderRadius: '4px',
+      fontSize: '1rem',
+      cursor: 'pointer',
+      transition: 'background-color 0.3s',
+    },
+    buttonHover: {
+      backgroundColor: '#535bf2',
+    },
+  };
+
   return (
-    <div>
-      <button onClick={handleGoogleSignIn}>Inicia sesión con Google</button>
-      {/* Otros elementos de tu componente */}
+    <div style={styles.container}>
+      <h1 style={styles.title}>Bienvenido a la Pokedex</h1>
+      <p style={styles.description}>
+        La Pokedex es una enciclopedia virtual que contiene información detallada sobre las diferentes especies de Pokémon. Aquí puedes explorar, descubrir y aprender más sobre cada Pokémon.
+      </p>
+      <button 
+        style={styles.button}
+        onMouseOver={(e) => e.target.style.backgroundColor = styles.buttonHover.backgroundColor}
+        onMouseOut={(e) => e.target.style.backgroundColor = styles.button.backgroundColor}
+        onClick={handleGoogleSignIn}
+      >
+        Inicia sesión con Google
+      </button>
     </div>
   );
 };
+
+export default Login;
